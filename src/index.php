0b43e5f2ecf66ec695402b0e99d56f4bd9c05ed8
Rafael S. Ferrari <rafael@southerntucan.com>
2021-07-26 18:20:56 -0300

    adding nginx and php services

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello there</title>
+        <style>
+            .center {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 50%;
+            }
+        </style>
+    </head>
+    <body>
+        <img src="https://media.giphy.com/media/WpIPS0DWNpMm4kfMVr/giphy.gif" alt="Hello there" class="center">
+    </body>
+</html>
